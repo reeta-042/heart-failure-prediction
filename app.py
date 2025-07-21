a8934733f8038d078b898734e607e8dfc131ae19
reeta-042 <155265356+reeta-042@users.noreply.github.com>
2025-07-21 18:29:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,30 @@ import pickle
 import numpy as np
 
 # Load model and scaler
-model = pickle.load(open('heart_model.pkl', 'rb'))
+model = pickle.load(open('best_log_model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))  # Remove this if you didn't use a scaler
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('heart_form.html')
+    return render_template('heart_failure.html')
 
 @app.route('/predict', methods=['POST'])
 def predict_risk():
     # Collect input from form
     age = float(request.form.get('age'))
-    anaemia = float(request.form.get('anaemia'))
-    creatinine_phosphokinase = float(request.form.get('creatinine_phosphokinase'))
-    diabetes = float(request.form.get('diabetes'))
-    ejection_fraction = float(request.form.get('ejection_fraction'))
-    high_blood_pressure = float(request.form.get('high_blood_pressure'))
+    anaemia = int(request.form.get('anaemia'))
+    creatinine_phosphokinase = int(request.form.get('creatinine_phosphokinase'))
+    diabetes = int(request.form.get('diabetes'))
+    ejection_fraction = int(request.form.get('ejection_fraction'))
+    high_blood_pressure = int(request.form.get('high_blood_pressure'))
     platelets = float(request.form.get('platelets'))
     serum_creatinine = float(request.form.get('serum_creatinine'))
     serum_sodium = float(request.form.get('serum_sodium'))
-    sex = float(request.form.get('sex'))
-    smoking = float(request.form.get('smoking'))
-    time = float(request.form.get('time'))
+    sex = int(request.form.get('sex'))
+    smoking = int(request.form.get('smoking'))
+    time = int(request.form.get('time'))
 
     # Prepare input
     features = [age, anaemia, creatinine_phosphokinase, diabetes, ejection_fraction,
@@ -42,9 +42,9 @@ def predict_risk():
 
     # Interpret result
     if prediction[0] == 1:
-        result = "🚨 Patient is at risk of heart failure. Begin treatment in earnest."
+        result = "Patient is at risk of heart failure. Begin treatment in earnest."
     else:
-        result = "✅ Patient is NOT at risk of heart failure."
+        result = "Patient is NOT at risk of heart failure."
 
     # Send result to template
     return render_template('heart_form.html', result=result)
